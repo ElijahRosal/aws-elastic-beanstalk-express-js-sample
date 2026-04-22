2b14ba4805dc7e7558ce5dfe851e87b5e49e2b94
ElijahRosal <elijah,rosal04@gmail.com>
2026-04-21 23:07:05 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is for CS4650'));
+app.get('/', (req, res) => res.send('This is how it works you know?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
